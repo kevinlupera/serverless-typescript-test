5935d6d4d6db0bc1b7ad8afb84fb7d3fb42a3c6e
kevin-lupera <kevinlupera@gmail.com>
2023-03-10 16:31:53 -0500

    Inicio de proyecto

---
+++ serverless.ts
@@ -0,0 +1,54 @@
+/* eslint-disable no-template-curly-in-string */
+import type { AWS } from '@serverless/typescript';
+
+import { hello } from './src/functions';
+
+const serverlessConfiguration: AWS = {
+  service: 'serverless-typescript',
+  frameworkVersion: '2',
+  useDotenv: true,
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true,
+      packager: 'yarn',
+    },
+    stage: '${opt:stage, self:provider.stage}',
+  },
+  plugins: [
+    'serverless-webpack',
+    'serverless-offline',
+  ],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    iamRoleStatements: [
+      {
+        Effect: 'Allow',
+        Action: ['codedeploy:*'],
+        Resource: '*',
+      },
+    ],
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+      metrics: false, // activate to see CacheHits and Misses
+    },
+    logs: {
+      // activate to see API Gateway logs
+      restApi: {
+        accessLogging: false,
+        executionLogging: false,
+        level: 'INFO',
+        fullExecutionData: false,
+      },
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+    lambdaHashingVersion: '20201221',
+  },
+  functions: { hello },
+};
+
+module.exports = serverlessConfiguration;
